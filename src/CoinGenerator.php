38f64f63429740d4b4fc586c6dea55c34dfd1327
Yunus Emre Deligöz <ye@deligoz.me>
2022-03-05 02:10:27 +0300

    CoinGenerator: Extends from `phonyland/framework` Generator

--- src/CoinGenerator.php
+++ src/CoinGenerator.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Phonyland\CoinGenerator;
 
-use Phonyland\GeneratorManager\Generator;
+use Phonyland\Framework\Generator;
 
 class CoinGenerator extends Generator
 {
